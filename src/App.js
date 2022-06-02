0928263ff1d657b22a56ae4817949d012563b18b
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-06-02 18:03:07 +0300

    Change app.js to class component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import './App.css';
-import Basket from './components/Basket';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
 import data from "./data.json";
-
 import Products from './components/Products';
-
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Shooping cart</a>
-      </header>
-      <main>
-        <div className="content">
+import React from "react";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
             <div className="main">
-              <Products products={data.products} />
+              <Products products={this.state.products}></Products>
             </div>
             <div className="sidebar">Cart Items</div>
-        </div>
-      </main>
-      <footer>
-        Femi Adesola 
-      </footer>
-    </div>
-  );
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
+
 
