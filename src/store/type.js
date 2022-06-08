38d395cd99a3fdf23ff19292943797e258467707
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-06-08 16:02:21 +0300

    creates orderReducer, orderAction and order api in server.js

--- src/store/type.js
+++ src/store/type.js
@@ -1,5 +1,12 @@
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
 export const FILTER_PRODUCTS_BY_SIZE = "FILTER_PRODUCTS_BY_SIZE";
 export const ORDER_PRODUCTS_BY_PRICE = "ORDER_PRODUCTS_BY_PRICE";
+
+// For adding and removing from cart 
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
+// For creating order 
+export  const CREATE_ORDER = "CREATE_ORDER";
+export  const CLEAR_ORDER = "CLEAR_ORDER";
+export  const CLEAR_CART= "CLEAR_CART";
